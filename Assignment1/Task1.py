6b9ba4be17fcd2ff702dca82d9c1af4a68d926df
patrikpeterson69 <h23patpe@du.se>
2025-12-17 10:22:08 +0100

    Update Task1.py
    examåple

---
+++ Assignment1/Task1.py
@@ -0,0 +1,21 @@
+# Has multi-dimensional arrays and matrices.
+# Has a large collection of mathematical functions to operate on these arrays.
+import numpy as np
+
+# Data manipulation and analysis.
+import pandas as pd
+
+# Data visualization tools.
+import seaborn as sns
+
+import mesa
+
+class MoneyAgent(mesa.Agent):
+    """An agent with fixed initial wealth."""
+
+    def __init__(self, model):
+        # Pass the parameters to the parent class.
+        super().__init__(model)
+
+        # Create the agent's variable and set the initial values.
+        self.wealth = 1
