00de2dddc0ad6ae6ae1dd4d2e63f8373140f1172
Davidskaring <h23davsk@du.se>
2025-12-18 10:57:11 +0100

    Update Task1.py

--- Assignment1/Task1.py
+++ Assignment1/Task1.py
@@ -1,43 +1,135 @@
-import mesa
+#Has multi-dimensional arrays and matrices.
+# Has a large collection of mathematical functions to operate on these arrays.
+import numpy as np
 
-class MoneyAgent(mesa.Agent):
+# Data manipulation and analysis.
+import pandas as pd
+
+# Data visualization tools.
+import seaborn as sns
+
+import mesa
+from mesa import space
+from mesa.discrete_space import CellAgent, OrthogonalMooreGrid
+from mesa.visualization import SolaraViz, SpaceRenderer, make_plot_component
+from mesa.visualization.components import AgentPortrayalStyle
+from mesa.space import MultiGrid
+
+
+
+def compute_gini(model):
+    agent_wealths = [agent.wealth for agent in model.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum(xi * (N - i) for i, xi in enumerate(x)) / (N * sum(x))
+    return 1 + (1 / N) - 2 * B
+
+# CARAGENT FOR MOVING AND FINDING PARKING SPACE
+class CarAgent(CellAgent):
     """An agent with fixed initial wealth."""
 
-    def __init__(self, model):
-        # Pass the parameters to the parent class.
-        super().__init__(model)
+    def __init__(self, model, cell):
+        """initialize a MoneyAgent instance.
 
-        # Create the agent's variable and set the initial values.
+        Args:
+            model: A model instance
+        """
+        super().__init__(model)
+        self.paused = False
+        self.cell = cell
         self.wealth = 1
 
-    def exchange(self):
-        # Verify agent has some wealth
-        if self.wealth > 0:
-            other_agent = self.random.choice(self.model.agents)
-            if other_agent is not None:
-                other_agent.wealth += 1
-                self.wealth -= 1
+    def move(self):
+        """Move the agent to a random neighboring cell."""
+        self.cell = self.cell.neighborhood.select_random_cell()
+        
+    def pause(self):
+        self.paused = True
+    
+    def unpause(self):
+        self.paused = False
+        
 
 
-class MoneyModel(mesa.Model):
-    """A model with some number of agents."""
+    def park(self):
+        """Give 1 unit of wealth to a random agent in the same cell."""
+        # pausa om en
+        cellmates = [a for a in self.cell.agents if a is not self]
 
-    def __init__(self, n):
-        super().__init__()
-        self.num_agents = n
-
-        # Create agents
-        MoneyAgent.create_agents(model=self, n=n)
+        if cellmates:  # Only give money if there are other agents present
+            self.paused = True
 
     def step(self):
-        """Advance the model by one step."""
-        # This function psuedo-randomly reorders the list of agent objects and
-        # then iterates through calling the function passed in as the parameter
-        self.agents.shuffle_do("exchange")
+        """do one step of the agent."""
+        if self.paused:
+            return
+        self.move()
 
-        # Execute the model
+        
 
-model = MoneyModel(10)
-model.step()
-# Make sure it worked
-print(f"You have {len(model.agents)} agents.")
+
+class ParkAgent(CellAgent):
+    """An agent with fixed initial wealth."""
+
+    def __init__(self, model, cell):
+        """initialize a Parkagent instance.
+
+        Args:
+            model: A model instance
+        """
+        super().__init__(model)
+        self.cell = cell
+
+    def move(self):
+        """Move the agent to a random neighboring cell."""
+        self.cell = self.cell.neighborhood.select_random_cell()
+
+#    def give_money(self):
+#        """Give 1 unit of wealth to a random agent in the same cell."""
+#        cellmates = [a for a in self.cell.agents if a is not self]
+#
+#        if cellmates:  # Only give money if there are other agents present
+#            other = self.random.choice(cellmates)
+#            other.wealth += 1
+#            self.wealth -= 1
+
+
+
+
+
+
+
+
+
+class ParkingModel(mesa.Model):
+    """A model with some number of agents."""
+
+    def __init__(self, n=10, width=10, height=10, seed=None):
+        """Initialize a MoneyModel instance.
+
+        Args:
+            N: The number of agents.
+            width: width of the grid.
+            height: Height of the grid.
+        """
+        super().__init__(seed=seed)
+        self.num_agents = n
+        self.grid = OrthogonalMooreGrid((width, height), random=self.random)
+        # Create agents
+        CarAgent.create_agents(
+            self,
+            self.num_agents,
+            self.random.choices(self.grid.all_cells.cells, k=self.num_agents),
+        )
+
+        self.datacollector = mesa.DataCollector(
+            model_reporters={"Gini": compute_gini}, agent_reporters={"Wealth": "wealth"}
+        )
+        self.datacollector.collect(self)
+
+
+    def step(self):
+        """do one step of the model"""
+        self.agents.shuffle_do("step")
+        self.datacollector.collect(self)
+
