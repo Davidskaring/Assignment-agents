d577fef36911977d6c10c55e5724071811e4cd7d
Eriktysklind <h23eribl@du.se>
2025-12-19 15:10:09 +0100

    .

--- Assignment1/Task2.py
+++ Assignment1/Task2.py
@@ -1,60 +1,117 @@
 import random
+from mesa import Model, DataCollector
+from mesa.agent import AgentSet
+from mesa.discrete_space import CellAgent, OrthogonalMooreGrid
+from numpy.ma.core import append
 
-from mesa.discrete_space import CellAgent
 
-"""
-1.
-Create 50 tasks with varying duration and resources.
-Example
-Task 1: Duration = 10 units, Resources = 2 (requires 2 agents to work together).
-Task 2: Duration = 15 units, Resources = 3 (requires 3 agents to work together).
-Task 3: Duration = 17 units, Resources = 1 (requires 1 agent).
-"""
-class Task:
-    def __init__(self, id, duration, resources):
-        self.id = id
-        self.duration = duration
-        self.resources = resources
-
-    def __repr__(self):
-        return f"Uppgift {self.id}: Tid={self.duration}, Kräver={self.resources} pers"
-
-task_list = []
-
-for i in range(50): # [cite: 26]
-    # Slumpa tid mellan 5 och 15 sekunder
-    tid = random.randint(5, 15)
-
-    # Slumpa resurser: Antingen 1, 2 eller 3 personer behövs
-    resurser = random.randint(1, 3)
-
-    # här skapar vi en task
-    ny_uppgift = Task(i, tid, resurser)
-
-    # lägger vi lappen i högen
-    task_list.append(ny_uppgift)
-    # Titta på de 5 första lapparna ---
-    print("Här är de 5 första uppgifterna i högen:")
-    for task in task_list:
-        print(task)
-
-#Här skapar vi agenterna
-
-class WorkerAgent(CellAgent):
-    def __init__(self, cell, model, capacity):
+# ---------------------------------------------------------
+# 1. AGENTEN
+# ---------------------------------------------------------
+class WorkerAgent1(CellAgent):
+    def __init__(self, model, cell, capacity=2):
         super().__init__(model)
-        self.id = id
         self.cell = cell
-
-        #hur många jobb agenten har "kapacitet"
         self.capacity = capacity
+        self.my_jobs = []  # Lista för att hålla tasks
+        self.busy = False
+        self.stepcd = 0
 
-        #för att veta hur många jobb agenten har
-        self.my_jobs = []
+    def working(self):
 
-        #funktion för att kolla om agenten är ledig och kan jobba
+        for a in self.cell.agents:
+            if isinstance(a, TaskAgent) and not self.busy:
+                self.stepcd += self.duration
+
+
     def is_worker_available(self):
-        if self.my_jobs < self.capacity:
+
+        if len(self.my_jobs) < self.capacity:
             return True
         else:
             return False
+
+
+class WorkerAgent2(CellAgent):
+    def __init__(self, model, cell, capacity=1):
+        super().__init__(model)
+        self.cell = cell
+        self.capacity = capacity
+        self.my_jobs = []  # Lista för att hålla tasks
+
+
+class WorkerAgent3(CellAgent):
+    def __init__(self, model, cell, capacity=2):
+        super().__init__(model)
+        self.cell = cell
+        self.capacity = capacity
+        self.my_jobs = []  # Lista för att hålla tasks
+
+
+
+class TaskAgent(CellAgent):
+    """An agent with fixed initial wealth."""
+
+    def __init__(self, model, cell, id, duration, resources, task, tasklist):
+        super().__init__(model)
+        self.cell = cell
+        self.id = id
+        self.duration = random.randint(10,20)
+        self.resources = random.randint(1, 3)
+        self.task = 50
+        self.tasklist = []
+
+    def taskcreate(self):
+
+        for n in range (self.task):
+            tasks = self.duration , self.resources , self.id
+            self.tasklist.append(tasks)
+
+
+
+
+# 3. MODELLEN
+
+
+class SchedulerModel(Model):
+    def __init__(self, t=1, num_tasks=50):
+        super().__init__()
+        self.num_WrokerAgent1 = t
+        self.num_WrokerAgent2 = t
+        self.num_WrokerAgent3 = t
+        self.num_TaskAgents = num_tasks
+
+
+
+        WorkerAgent1.create_agents(
+            self,
+            self.num_WrokerAgent1,
+      # Skickas till agentens __init__
+        )
+
+        WorkerAgent2.create_agents(
+            self,
+            self.num_WrokerAgent2,
+            # Skickas till agentens __init__
+        )
+
+        WorkerAgent3.create_agents(
+            self,
+            self.num_WrokerAgent3,
+            # Skickas till agentens __init__
+        )
+
+        TaskAgent.create_agents(
+            self.sort(self),
+            self.num_TaskAgents,
+
+        )
+        self.grid = OrthogonalMooreGrid((5, 10), random=self.random)
+        self.datacollector = DataCollector(
+            # denna kommer från funktionen nedanför som räknar antal bilar som är parkerade
+            model_reporters={"Occupied Spots": self.count_occupied_spots},
+            # wealth får fungera som en av ovh på knapp för påsatta bilar
+            agent_reporters={"Wealth": "wealth"}
+        )
+
+
